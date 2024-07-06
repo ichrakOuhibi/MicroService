2e388bcb5b98c98c4cda2e5683728ad649335cf0
ICHRAK <ichrak.ouhibi@esprit.tn>
2024-07-06 23:40:57 +0100

    commit gateway

---
+++ src/main/java/com/example/gatwey/GatweyApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatwey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatweyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatweyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gatwey/GatweyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatwey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatweyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
